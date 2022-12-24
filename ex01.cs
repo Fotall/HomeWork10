13482535a0e3427926c07a5724a8e268b19a88af
Fotall <d.budarin@list.ru>
2022-12-24 15:02:47 +0300

    save3

--- ex01.cs
+++ ex01.cs
@@ -2,7 +2,7 @@
 // заполненный случайными вещественными числами.
 // m = 3, n = 4.
 // 0,5 7 -2 -0,2
-/*
+
 int[,] matrix = new int[3, 4];
 
 void PrintArray(int[,] matrix)
@@ -63,4 +63,3 @@ void FillArray(double[,] matrix)
 
 FillArray(matrix);
 PrintArray(matrix);
-*/
